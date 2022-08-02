f88dc38395bed89e83cad1983d41bb545e44dcdd
sadiasultananadisa <sadianafisa115@gmail.com>
2022-08-02 23:49:05 +0600

    ok

--- js/app.js
+++ js/app.js
@@ -84,7 +84,7 @@ $(document).ready(function(){
 $('.blog-Slider').slick({
   slidesToShow:3,
   slidesToScroll: 1,
-  autoplay:false,
+  autoplay:true,
   autoplaySpeed:2000,
   centerMode: true,
   centerPadding: "0px",
@@ -123,5 +123,55 @@ $('#num1').countMe(7,5);
 $('#num2').countMe(9,6);
 $('#num3').countMe(10,8);
 $('#num4').countMe(11,9);
-
 })
+$(document).ready(function(){
+  $(document).ready(function(){
+    $('.testimonial-slider').slick({
+      slidesToShow:1,
+      slidesToScroll: 1,
+      autoplay:true,
+      autoplaySpeed:2000,
+      centerMode: true,
+      centerPadding: "0px",
+      prevArrow:'.testimonial-left-arrow',
+      nextArrow:'.testimonial-right-arrow',
+      asNavFor: '.testimonial-img-slider',
+      /*responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: false,
+          },
+        },
+        {
+          breakpoint: 991,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          },
+        },
+        {
+          breakpoint: 575,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          },
+        },
+      ],*/
+    });
+    })
+
+  $('.testimonial-img-slider').slick({
+    slidesToShow: 3,
+    autoplay:true,
+    slidesToScroll: 1,
+    centerMode: true,
+    centerPadding: "0px",
+    prevArrow:'.testimonial-left-arrow',
+   nextArrow:'.testimonial-right-arrow',
+   asNavFor: '.testimonial-slider',
+  })
+});
