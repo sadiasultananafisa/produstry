8c7f889197b5a62cec6dbdb325067fc898143da6
sadiasultananadisa <sadianafisa115@gmail.com>
2022-08-03 23:02:45 +0600

    ok

--- js/app.js
+++ js/app.js
@@ -88,8 +88,8 @@ $('.blog-Slider').slick({
   autoplaySpeed:2000,
   centerMode: true,
   centerPadding: "0px",
-  prevArrow:'.blog-left-arrow',
-  nextArrow:'.blog-right-arrow',
+  prevArrow:'.blog-right-arrow',
+  nextArrow:'.blog-left-arrow',
   responsive: [
     {
       breakpoint: 1024,
@@ -124,7 +124,6 @@ $('#num2').countMe(9,6);
 $('#num3').countMe(10,8);
 $('#num4').countMe(11,9);
 })
-$(document).ready(function(){
   $(document).ready(function(){
     $('.testimonial-slider').slick({
       slidesToShow:1,
@@ -133,10 +132,10 @@ $(document).ready(function(){
       autoplaySpeed:2000,
       centerMode: true,
       centerPadding: "0px",
-      prevArrow:'.testimonial-left-arrow',
-      nextArrow:'.testimonial-right-arrow',
+      prevArrow:'.testimonial-right-arrow',
+      nextArrow:'.testimonial-left-arrow',
       asNavFor: '.testimonial-img-slider',
-      /*responsive: [
+      responsive: [
         {
           breakpoint: 1024,
           settings: {
@@ -160,10 +159,8 @@ $(document).ready(function(){
             slidesToScroll: 1,
           },
         },
-      ],*/
+      ],
     });
-    })
-
   $('.testimonial-img-slider').slick({
     slidesToShow: 3,
     autoplay:true,
@@ -175,3 +172,40 @@ $(document).ready(function(){
    asNavFor: '.testimonial-slider',
   })
 });
+$(document).ready(function(){
+  $('.brand-Slider').slick({
+    slidesToShow:5,
+    slidesToScroll: 1,
+    autoplay:true,
+    autoplaySpeed:2000,
+    centerMode: true,
+    centerPadding: "0px",
+    prevArrow:'.blog-right-arrow',
+    nextArrow:'.blog-left-arrow',
+    /*responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: false,
+        },
+      },
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 575,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],*/
+  });
+  })
