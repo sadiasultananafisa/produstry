1c72bc718a59565bcbb8d5397c942217ff4cec62
sadiasultananadisa <sadianafisa115@gmail.com>
2022-08-05 23:39:38 +0600

    okl

--- js/app.js
+++ js/app.js
@@ -173,37 +173,14 @@ $('#num4').countMe(11,9);
   })
 });
 $(document).ready(function(){
-  $('.brand-Slider').slick({
+  $('.brand-slider').slick({
     slidesToShow:5,
     slidesToScroll: 1,
     autoplay:true,
     autoplaySpeed:2000,
     centerMode: true,
     centerPadding: "0px",
-    /*responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: false,
-        },
-      },
-      {
-        breakpoint: 991,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 575,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],*/
+    prevArrow:'.brand-right-arrow',
+    nextArrow:'.brand-left-arrow',
   });
   })
