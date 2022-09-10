2f96a60e0acfcabfc4b232088eca34f899258727
sadiasultananadisa <sadianafisa115@gmail.com>
2022-09-10 11:20:27 +0600

    finished

--- js/app.js
+++ js/app.js
@@ -346,37 +346,23 @@ $(document).ready(function(){
   $(function(){
     var mixer = mixitup('.filtered-post');
   })
+})
+const counterUp = window.counterUp.default
 
+const callback = entries => {
+	entries.forEach( entry => {
+		const el = entry.target
+		if ( entry.isIntersecting && ! el.classList.contains( 'is-visible' ) ) {
+			counterUp( el, {
+				duration: 2000,
+				delay: 16,
+			} )
+			el.classList.add( 'is-visible' )
+		}
+	} )
+}
 
+const IO = new IntersectionObserver( callback, { threshold: 1 } )
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    })
+const el = document.querySelector( '#counter-up' )
+IO.observe( el )
